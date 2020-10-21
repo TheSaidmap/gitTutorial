893fdaee27b4e6a65513e34b1b137e172b322d35
TheSaidmap <43337557+TheSaidmap@users.noreply.github.com>
2020-10-21 21:55:30 +0300

    more files

---
+++ INSERT INTO.sql
@@ -0,0 +1,11 @@
+INSERT INTO book  VALUES ( 1, 'The Diary a Young Girl', '0199535566'),
+                         ( 2, 'Pride and Prejudice', '9780307594006'),
+						 ( 3, 'To Kill a Mockingbird', '0446310786'),
+						 ( 4, 'The Book of Gutsy Women: Favorite Stories of Courage and Resilience', '1501178415'),
+						 ( 5, 'War and Peace', '1788886526');
+						 
+INSERT INTO publsher  VALUES (1, 'Everyman''s Libary' , 'NY'),
+                              (2, 'Oxford University Press', 'NY'),
+							  (3, 'Grand Central Publishing', 'Washington'),
+							  (4, 'Simon & Schuster', 'Chicago');
+	
